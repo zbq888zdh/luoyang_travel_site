2577b9c421592ab10d5955d4777c0d4892149441
zbq888zdh <dibingqiong@gmail.com>
2026-05-23 01:50:38 +0700

    Update settings.py

--- luoyang_travel_site/settings.py
+++ luoyang_travel_site/settings.py
@@ -80,10 +80,6 @@ DATABASES = {
     'default': {
         'ENGINE':  'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'root',
-        'PASSWORD': '20001208',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
     }
 }
 
